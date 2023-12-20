79584176711086ea31357097762f2e5c5de19fc8
AkifAhmed5718 <122450551+AkifAhmed5718@users.noreply.github.com>
2023-12-20 16:51:52 +0100

    Initial commit

---
+++ BH.Web/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BH.Models;
+using BH.Models.ResponseModels;
+using BH.Models.ViewModels;
+using BH.Repositories;
+using BH.Services.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BH.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountRepo> _logger;
+        private readonly IAccountService _accountService;
+        public AccountController(ILogger<AccountRepo> logger, IAccountService accountService)
+        {
+            _logger = logger;
+            _accountService = accountService;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterUser(RegisterUserModel requestModel)
+        {
+            _logger.LogInformation("Going to execute Method: RegisterUser, Class: AccountController");
+            var result = await _accountService.RegisterUser(requestModel);
+            _logger.LogInformation("Execution completed Method: RegisterUser, Class: AccountController");
+
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginUserModel requestModel)
+        {
+            _logger.LogInformation("Going to execute Method: Login, Class: AccountController");
+            ResultModel<LoginResponse> result = await _accountService.LoginUser(requestModel);
+            _logger.LogInformation("Execution completed Method: Login, Class: AccountController");
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [Authorize]
+        [HttpGet]
+        public string Get()
+        {
+            return "You hit me!";
+        }
+    }
+}
+
---
+++ BH.Web/Program.cs
@@ -0,0 +1,138 @@
+﻿using BH.Repositories.Connections;
+using BH.Repositories.Constants;
+using BH.Web.Data;
+using BH.Services.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using BH.Web.ActionFilters;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+    configuration.RootPath = "ClientApp/build";
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{   
+    c.OperationFilter<AddRequiredHeaderParameter>();
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+var provider = builder.Services.BuildServiceProvider();
+var configuration = provider.GetRequiredService<IConfiguration>();
+var connections = new Dictionary<ConnectionName, string>
+{
+    {ConnectionName.BHDB, configuration.GetConnectionString(DBConstant.BH_DB_CONNECTION)}
+};
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connections[ConnectionName.BHDB]));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+builder.Services.AddAuthentication(auth =>
+{
+    auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        RequireExpirationTime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration.GetSection("Jwt:Issuer").Value,
+        ValidAudience = builder.Configuration.GetSection("Jwt:Audience").Value,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Jwt:Key").Value))
+    };
+});
+
+builder.Services.AddSingleton<IDictionary<ConnectionName, string>>(connections);
+builder.Services.AddGatewayDependencyGroup();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseSpaStaticFiles();
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseReactDevelopmentServer(npmScript: "start");
+    }
+    else
+    {
+        spa.Options.DefaultPageStaticFileOptions = new StaticFileOptions
+        {
+            OnPrepareResponse = context =>
+            {
+                // never cache index.html
+                if (context.File.Name == "index.html")
+                {
+                    context.Context.Response.Headers.Add("Cache-Control", "no-cache, no-store");
+                    context.Context.Response.Headers.Add("Expires", "-1");
+                }
+            }
+        };
+    }
+});
+
+app.Run();
+
